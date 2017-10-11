e234291196588f3fb49a4f6cb0fe8a1b46bb002c
sevensidedmarble <andrewianstewart@gmail.com>
2017-10-10 23:16:05 -0400

    minor bug fixing

--- app/models/player.rb
+++ app/models/player.rb
@@ -1,4 +1,10 @@
-module PlayerCommandSet
+class PlayerCommandSet
+  attr_accessor :cmds
+
+  def initialize
+    @cmds = [LookCommand.new, MoveCommand.new]
+  end
+
   class LookCommand
     attr_accessor :name, :aliases, :callback
 
@@ -8,23 +14,51 @@ module PlayerCommandSet
     end
 
     def initialize
+      @is_command = true
       @name = 'look'
       @aliases = ['l']
-      @callback = lambda { |user, input_string|
-        room = current_room
-        title = "A dusty plain."
-        desc = "It's dusty for miles."
+      @callback = lambda { |player, input_string|
+        room = player.current_room
+        title = room.title
+        desc = room.description
         return [title, desc]
       }
     end
   end
+
+  class MoveCommand
+    attr_accessor :name, :aliases, :callback
+
+    attr_accessor :keys
+    def keys
+      @aliases.push(@name)
+    end
+
+    def initialize
+      @is_command = true
+      @name = 'move'
+      @aliases = ['m']
+      @callback = lambda { |player, input_string|
+        # arguments = input_string.split(' ').drop(1)
+        # case arguments.first
+        #   when 'east'
+        #     # if there is an exit to the east
+        #     # then go east
+        #     # else return 'no exit there'
+        # end
+        # return []
+      }
+    end
+
+  end
+
 end
 
 class Player < ApplicationRecord
-  belongs_to :room, :user
+  belongs_to :room
+  belongs_to :user
 
-  include UserCommandSet
-  @@user_cmdset = [LookCommand.new]
+  @@player_cmds = PlayerCommandSet.new
 
   def current_room
     Room.find(self.room_id)
@@ -39,7 +73,7 @@ class Player < ApplicationRecord
     words = input_string.split(' ')
     # try to match the first word.
     first_word = words.first.to_s #
-    command = @@user_cmdset.find { |cmd| cmd.keys.include?(first_word) }
+    command = @@player_cmds.cmds.find { |cmd| cmd.keys.include?(first_word) }
     if command
       command.callback.call(self, input_string)
     else
