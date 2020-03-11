24e82b2abdedba82478d5c413c7562a512539d64
sevensidedmarble (Andrew Stewart) <andrewianstewart@gmail.com>
2020-03-10 21:30:40 -0400

    Add routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,22 @@
 Rails.application.routes.draw do
-  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  # TODO: Should the root show worlds instead?
+  # root 'worlds#show'
+
+  root to: redirect('play')
+  get 'play', to: 'worlds#show', as: 'play'
+
+  get 'signup', to: 'users#new', as: 'signup'
+  match 'login', to: 'sessions#create', as: 'login', via: %i[get post]
+  get 'logout', to: 'sessions#destroy', as: 'logout'
+
+  get 'home', to: 'pages#home', as: 'home'
+
+  resources :users
+  resources :sessions
+
+  get '/' => redirect('play')
+
+  mount ActionCable.server => '/cable'
+  # TODO: Work on below
+  # match "/telnet" => TelnetSinatra
 end
